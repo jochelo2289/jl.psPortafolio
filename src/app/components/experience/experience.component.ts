fd37179f9f5ce234986587caf3cfee5c74474ca7
Jose Luis Parra Sánchez <jl.ps@hotmail.com>
2018-02-17 08:21:23 -0500

    Componente de experiencia terminado.

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperienceComponent implements OnInit {
 
+  public experiencia = 0;
+
   constructor() { }
 
   ngOnInit() {
+    const year_init = 2014;
+    const year_today = new Date().getFullYear();
+
+    this.experiencia = year_today - year_init;
   }
 
 }
